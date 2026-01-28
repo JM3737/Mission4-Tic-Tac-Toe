cb89a40b348ebcc067d9f6ab01e367ac65fec367
JM3737 <joshua1110041331@gmail.com>
2026-01-28 13:44:25 -0700

    Added names for the necessary methods.

--- Support.cs
+++ Support.cs
@@ -6,5 +6,16 @@ namespace Mission4_Tic_Tac_Toe
 {
     internal class Support
     {
+        // Method that prints the board based on the information passed to it
+        public void printBoard(char[,] board)
+        {
+            // Code here
+        }
+
+        // Method that receives the game board array as input and returns if there is a winner and who it was
+        public char checkWinner(char[,] board)
+        {
+            // Code here
+        }
     }
 }
